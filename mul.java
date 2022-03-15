f931fe52467beedd1c5187d9472b16d89851926b
rashmisid <101619054+rashmisid@users.noreply.github.com>
2022-03-15 12:56:38 +0530

    Add files via upload

---
+++ mul.java
@@ -0,0 +1,13 @@
+class mul
+{
+static int multiply (int n1,int n2,int n3)
+{int result =n1*n2*n3;
+System.out.println(result);
+return result;
+}
+
+public static void main(String ar[])
+{
+multiply(7,6,5);
+
+}}
